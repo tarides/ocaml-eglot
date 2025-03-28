bb8318cb5ab732bb16d8a6321f91be43c94229e6
xvw <xavier.vdw@gmail.com>
2025-03-28 13:28:27 +0100

    Upgrade version to `1.1`

--- ocaml-eglot.el
+++ ocaml-eglot.el
@@ -13,7 +13,7 @@
 ;; Keywords: ocaml languages
 ;; URL: https://github.com/tarides/ocaml-eglot
 ;; Package-Requires: ((emacs "29.1"))
-;; Package-Version: 1.0
+;; Package-Version: 1.1
 ;; SPDX-License-Identifier: MIT
 
 ;;; Commentary:
